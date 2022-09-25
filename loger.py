316a70a007d57122a5d5fccbe304027932c81431
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-25 21:50:30 +0300

    put the datetime in the first place

--- loger.py
+++ loger.py
@@ -2,6 +2,5 @@ from datetime import datetime
 
 def dif_log(str):
     with open('surveillance.txt', 'a+', encoding='UTF-8') as file:
-        file.write(f'{str} --- {datetime.now}\n')
+        file.write(f'{datetime.now}:  {str}\n')
         
-#

2e48f572b562247ed44201bb3cd8d4255089de1f
VladimirS0l <vbnm1993@yandex.ru>
2022-09-26 22:10:42 +0300

    check1

--- check.py
+++ check.py
@@ -1,1 +1,49 @@
 
+# def user_mode(rez: str) -> bool:
+#     return rez == '1' or rez == '2' or rez == '3'
+
+from queue import Empty
+
+rez = input('Enter: ')
+def foo(rez: str) -> bool:
+   return rez == Empty
+
+print(foo(rez))
+
+
+
+
+
+
+
+# def empty_line(rez2):
+
+
+# def is_compl(rez3):
+
+
+
+
+# def is_real_number(inputed):
+#     is_correct = False
+#     while not is_correct:
+#         try:
+#             number = int(input(f'{inputed}'))
+#             is_correct = True
+#         except ValueError:
+#             print('Некорректный ввод')
+#     return number
+
+# def is_complex_number(inputed):
+#     is_correct = False
+#     while not is_correct:
+#         try:
+#             number = int(input(f'{inputed}'))
+#             is_correct = True
+#         except ValueError:
+#             print('Некорректный ввод')
+#     return number
+
+# def is_evel():
+#     eval()
+

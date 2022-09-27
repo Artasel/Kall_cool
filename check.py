be04c500e44da820d4421ed544f24ddf3512aff8
VladimirS0l <vbnm1993@yandex.ru>
2022-09-27 11:40:03 +0300

    check finish

--- check.py
+++ check.py
@@ -1,20 +1,34 @@
 
-# def user_mode(rez: str) -> bool:
-#     return rez == '1' or rez == '2' or rez == '3'
+def user_mode(rez: str) -> bool:
+    return rez == '1' or rez == '2' or rez == '3'
+
 '''проверка пользователя на ввод в меню'''
 
-# def empty_line(rez: str) -> bool:
-#     s = len(rez)
-#     return s == 0
+def empty_line(rez: str) -> bool:
+    s = len(rez)
+    return s == 0
+
 '''проверка ввода пустой строки'''
 
-num = '1.2 4.3'
-num1 = 0
 def is_compl(num):
-    print(num)
-    num1 = num.split(' ')
-    print(num1)
-is_compl(num)
+    num1, num2 = num.split(' ')
+    return num1.isdigit(), num2.isdigit()
+    
+'''проверка и разбитие строки на 2 значения'''
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+'''проверяет число на float'''
+
+
+
+
+
 
 
 

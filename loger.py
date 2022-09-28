1ecb02ce5aa462a2715e41da8fe81aa14b7d2a1e
Любовь Кравченко <kravchenkoluba@yandex.ru>
2022-09-28 11:30:20 +0300

    changed the data and time output

--- loger.py
+++ loger.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
-def dif_log(str):
-    with open('surveillance.txt', 'a+', encoding='UTF-8') as file:
-        file.write(f'{datetime.now}:  {str}\n')
+def dif_log(x):
+    time = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+    with open('surveillance.txt', 'a', encoding='UTF-8') as file:
+        file.write(f'{time}:  {x}\n')
         
+x = 'ghfjutgjtgot'
+print(dif_log(x))
